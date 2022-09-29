09326d31cf330ba60c443aabaf91dd3a108275ba
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2022-09-29 14:23:48 +0530

    Time: 107 ms (52.30%), Space: 31.8 MB (71.10%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -2,6 +2,8 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        /* brute force O(nlogn)
+        
         priority_queue<pp, vector<pp>, greater<pp>> pq;
         for(auto i: arr){
             pq.push({abs(i-x),i});
@@ -12,6 +14,16 @@ public:
             pq.pop();
         }
         sort(ans.begin(),ans.end());
+        return ans;*/
+        
+        /* Little Optimized O(n)*/
+        int l=0,r=arr.size()-1;
+        while(r-l>=k){
+            if(abs(arr[l]-x)<=abs(arr[r]-x)) r--;
+            else l++;
+        }
+        vector<int> ans;
+        for(int i=l; i<=r; i++) ans.push_back(arr[i]);
         return ans;
     }
 };
