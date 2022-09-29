0ba3717a2562361a66b85d30f80aa41f58b6621c
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2022-09-29 22:25:00 +0530

    Time: 46 ms (93.47%), Space: 31.1 MB (86.01%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,7 +1,7 @@
 #define pp pair<int,int>
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    vector<int> findClosestElements(vector<int>& A, int k, int x) {
         /* brute force O(nlogn)
         
         priority_queue<pp, vector<pp>, greater<pp>> pq;
@@ -17,13 +17,23 @@ public:
         return ans;*/
         
         /* Little Optimized O(n)*/
-        int l=0,r=arr.size()-1;
-        while(r-l>=k){
-            if(abs(arr[l]-x)<=abs(arr[r]-x)) r--;
-            else l++;
+//         int l=0,r=arr.size()-1;
+//         while(r-l>=k){
+//             if(abs(arr[l]-x)<=abs(arr[r]-x)) r--;
+//             else l++;
+//         }
+//         vector<int> ans;
+//         for(int i=l; i<=r; i++) ans.push_back(arr[i]);
+//         return ans;
+        
+        int left = 0, right = A.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
         }
-        vector<int> ans;
-        for(int i=l; i<=r; i++) ans.push_back(arr[i]);
-        return ans;
+        return vector<int>(A.begin() + left, A.begin() + left + k);
     }
 };
