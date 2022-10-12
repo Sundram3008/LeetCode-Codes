3a406d9f86c718bd4ea9326afb3af33290996269
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2022-10-12 11:20:33 +0530

    Time: 395 ms (5.05%), Space: 153.7 MB (5.05%) - LeetHub

---
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string rankTeams(vector<string>& votes) {
+        if(votes.size()==1) return votes[0];
+        vector<vector<pair<int,char>>> arr(26);
+        for(int i=0; i<26; i++){
+            vector<pair<int,char>> temp;
+            arr[i]=temp;
+        }
+        for(int i=0; i<votes.size(); i++){
+            for(int j=0; j<votes[i].size(); j++)
+                arr[votes[i][j]-'A'].push_back(make_pair(j,votes[i][j]));
+        }
+        for(int i=0; i<26; i++){
+            sort(arr[i].begin(), arr[i].end());
+        }
+        vector<pair<string,char>> v(26);
+        string ans="";
+        for(int i=0; i<26; i++){
+            // for(auto j:arr[i]) cout<<j.first<<" "<<j.second<<" ";
+            // cout<<endl;
+            for(auto j: arr[i]){
+                v[i].first= v[i].first+ to_string(j.first);
+                v[i].second= j.second;
+            }
+        }
+        sort(v.begin(),v.end());
+        for(auto i: v){
+            //cout<<i.first<<" "<<i.second<<endl;
+            if(i.first.size()>0){
+                ans+= i.second;
+            }
+        }
+        return ans;
+    }
+};
