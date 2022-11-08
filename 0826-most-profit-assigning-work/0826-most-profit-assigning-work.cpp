24cea1428616b16151ca5210e487ca6a63721a6f
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2022-11-08 18:39:08 +0530

    Time: 139 ms (73.86%), Space: 37.5 MB (83.75%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& diff, vector<int>& pro, vector<int>& w) {
-        map<int,int> mp;
-        mp[0]=0;
-        for(int i=0; i<diff.size(); i++){
-            if(mp.find(diff[i])!= mp.end()) mp[diff[i]]= max(mp[diff[i]], pro[i]);
-            else mp[diff[i]]= pro[i];
-        }
-        int maxi= mp.begin()->second;
-        for(auto &i: mp){
-            if(i.second> maxi) maxi=i.second;
-            else i.second= maxi;
-        }
-        int ans=0;
-        for(auto i : w){
-            if(mp.find(i)!=mp.end()) ans+= mp[i];
-            else {
-                mp[i]=1000;
-                auto x= --mp.find(i);
-                ans+= x->second;
-                mp.erase(i);
+        //Good But better is next one
+        // map<int,int> mp;
+        // mp[0]=0;
+        // for(int i=0; i<diff.size(); i++){
+        //     if(mp.find(diff[i])!= mp.end()) mp[diff[i]]= max(mp[diff[i]], pro[i]);
+        //     else mp[diff[i]]= pro[i];
+        // }
+        // int maxi= mp.begin()->second;
+        // for(auto &i: mp){
+        //     if(i.second> maxi) maxi=i.second;
+        //     else i.second= maxi;
+        // }
+        // int ans=0;
+        // for(auto i : w){
+        //     if(mp.find(i)!=mp.end()) ans+= mp[i];
+        //     else {
+        //         mp[i]=1000;
+        //         auto x= --mp.find(i);
+        //         ans+= x->second;
+        //         mp.erase(i);
+        //     }
+        // }
+        // return ans;
+        vector<pair<int,int>> v;
+        int n= diff.size(), ans=0, j=0,maxi=0;
+        for(int i=0; i<n; i++) v.push_back({diff[i],pro[i]});
+        sort(v.begin(), v.end());
+        sort(w.begin(), w.end());
+        for(auto i: w){
+            while(j<n && v[j].first<=i){
+                maxi= max(maxi, v[j++].second);
             }
+            ans+= maxi;
         }
         return ans;
     }
