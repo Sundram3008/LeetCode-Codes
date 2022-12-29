c17c8a292d96edd076793ac2a6ad32b7636d2d6c
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2022-12-29 18:39:34 +0530

    Added solution - LeetHub

---
+++ Asteroid Collision - GFG/asteroid-collision.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> asteroidCollision(int n, vector<int> &a) {
+        // code here
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            if(a[i]>0) st.push(a[i]);
+            else {
+                while(!st.empty() && st.top()>0 && st.top()<abs(a[i])) st.pop();
+                if(!st.empty()&& st.top()==abs(a[i])) st.pop();
+                else if(!st.empty() && st.top()> abs(a[i])) continue;
+                else st.push(a[i]);
+            }
+        }
+        vector<int> ans;
+        while(!st.empty()) ans.push_back(st.top()), st.pop();
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> asteroids(N);
+        for (int i = 0; i < N; i++) cin >> asteroids[i];
+
+        Solution obj;
+        vector<int> ans = obj.asteroidCollision(N, asteroids);
+        for (auto &it : ans) cout << it << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
