5e834d5cc70feb17600385989716ce6321c0f809
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-02-21 12:54:03 +0530

    Time: 32 ms (43.92%), Space: 22.4 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,8 +6,8 @@ public:
             int mid = l+(r-l)/2;
             int x = (mid%2==0)? mid+1: mid-1;
             if(nums[mid]==nums[x]) l= mid+1;
-            else r= mid-1;
-            if(mid-1>=0 && mid+1<=nums.size()-1 && nums[mid]!= nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
+            else r= mid;
+            //if(mid-1>=0 && mid+1<=nums.size()-1 && nums[mid]!= nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
         }
         return nums[l];
     }
