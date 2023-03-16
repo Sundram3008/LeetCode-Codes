6eb02947f7a342ab94aca8aed9a63649c080a286
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-03-16 11:44:22 +0530

    Time: 66 ms (5.54%), Space: 40.7 MB (5.20%) - LeetHub

---
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* make(vector<int> &pre, int &idx, unordered_map<int,int> postMap){
+        if( idx>= pre.size() || postMap.size()==0) return NULL;
+        if(postMap.find(pre[idx])==postMap.end()) return NULL;
+        int newIdx= postMap[pre[idx]];
+        TreeNode * root= new TreeNode(pre[idx]);
+        idx++;
+        unordered_map<int,int> newMap;
+        for(auto i: postMap){
+            if(i.second<newIdx){
+                newMap[i.first]=i.second;
+            }
+        }
+        root->left= make(pre, idx, newMap);
+        root->right= make(pre, idx, newMap);
+        return root;
+        
+    }
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        unordered_map<int, int> mp;
+        int idx=0;
+        for(int i=0; i<postorder.size(); i++) mp[postorder[i]]=i;
+        return make(preorder, idx, mp);
+    }
+};
