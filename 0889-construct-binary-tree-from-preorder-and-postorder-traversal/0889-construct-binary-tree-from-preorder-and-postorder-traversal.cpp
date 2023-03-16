62f40de74c2db6df37412cc136e56f6ec5760371
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-03-16 12:02:04 +0530

    Time: 4 ms (97.32%), Space: 25.4 MB (92.11%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -11,27 +11,38 @@
  */
 class Solution {
 public:
-    TreeNode* make(vector<int> &pre, int &idx, unordered_map<int,int> postMap){
-        if( idx>= pre.size() || postMap.size()==0) return NULL;
-        if(postMap.find(pre[idx])==postMap.end()) return NULL;
-        int newIdx= postMap[pre[idx]];
-        TreeNode * root= new TreeNode(pre[idx]);
-        idx++;
-        unordered_map<int,int> newMap;
-        for(auto i: postMap){
-            if(i.second<newIdx){
-                newMap[i.first]=i.second;
-            }
-        }
-        root->left= make(pre, idx, newMap);
-        root->right= make(pre, idx, newMap);
-        return root;
+//     TC- O(n^2), SC- O(n)
+//     TreeNode* make(vector<int> &pre, int &idx, unordered_map<int,int> postMap){
+//         if( idx>= pre.size() || postMap.size()==0) return NULL;
+//         if(postMap.find(pre[idx])==postMap.end()) return NULL;
+//         int newIdx= postMap[pre[idx]];
+//         TreeNode * root= new TreeNode(pre[idx]);
+//         idx++;
+//         unordered_map<int,int> newMap;
+//         for(auto i: postMap){
+//             if(i.second<newIdx){
+//                 newMap[i.first]=i.second;
+//             }
+//         }
+//         root->left= make(pre, idx, newMap);
+//         root->right= make(pre, idx, newMap);
+//         return root;
         
-    }
+//     }
+//     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+//         unordered_map<int, int> mp;
+//         int idx=0;
+//         for(int i=0; i<postorder.size(); i++) mp[postorder[i]]=i;
+//         return make(preorder, idx, mp);
+//     }
+    
+    int pre=0, post=0;
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-        unordered_map<int, int> mp;
-        int idx=0;
-        for(int i=0; i<postorder.size(); i++) mp[postorder[i]]=i;
-        return make(preorder, idx, mp);
+        TreeNode* root= new TreeNode(preorder[pre++]);
+        if(root->val!=postorder[post]) root->left= constructFromPrePost(preorder, postorder);
+        if(root->val!=postorder[post]) root->right= constructFromPrePost(preorder, postorder);
+        post++;
+        return root;
     }
+    
 };
