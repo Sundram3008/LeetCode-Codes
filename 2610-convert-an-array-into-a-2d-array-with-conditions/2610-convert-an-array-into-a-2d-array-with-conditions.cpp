6717f7e57bcf872ea10b2424ee94217ffeca8323
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-04-02 09:44:37 +0530

    Time: 33 ms (12.50%), Space: 29.6 MB (12.50%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -3,9 +3,9 @@ public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
         unordered_map<int,int> mp;
         int n=0;
-        for(auto i: nums) mp[i]++, n= max(n, mp[i]);
+        for(auto &i: nums) mp[i]++, n= max(n, mp[i]);
         vector<vector<int>> ans(n);
-        for(auto i: mp){
+        for(auto &i: mp){
             for(int j=0; j<i.second; j++){
                 ans[j].push_back(i.first);
             }
