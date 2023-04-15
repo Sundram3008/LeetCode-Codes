4036f4b99012aec1e73ff71cc8f52efb66fdab1d
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-04-15 21:50:03 +0530

    Time: 848 ms (25.00%), Space: 365.3 MB (25.00%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -12,55 +12,86 @@
 #define pp pair<TreeNode*,int>
 class Solution {
 public:
+//     TreeNode* replaceValueInTree(TreeNode* root) {
+//         queue<pp> q;
+//         q.push({root, -1});
+        
+//         unordered_map<int, unordered_map<int,int>> mp;
+        
+//         int l=0, no=-1;
+        
+//         vector<int> lvls;
+        
+//         while(!q.empty()){
+            
+//             int n= q.size(), sum=0;
+            
+//             for(int i=0; i<n; i++){
+//                 TreeNode* a= q.front().first;
+//                 int b= q.front().second;
+               
+//                 mp[l][b]+= a->val;
+                
+//                 sum+= a->val;
+//                 no++;
+//                 if(a->left!=NULL) q.push({a->left, no});
+//                 if(a->right!=NULL) q.push({a->right, no});
+//                 q.pop();
+                
+//             }
+//             lvls.push_back(sum);
+//             l++;
+//         }
+        
+//         q.push({root, -1});
+//         l=0, no=-1;
+//         while(!q.empty()){
+//             int n= q.size(), sum=0;
+            
+//             for(int i=0; i<n; i++){
+//                 TreeNode* a= q.front().first;
+//                 int b= q.front().second;
+               
+//                 a->val= lvls[l]- mp[l][b];
+//                 no++;
+//                 if(a->left!=NULL) q.push({a->left, no});
+//                 if(a->right!=NULL) q.push({a->right, no});
+//                 q.pop();
+//             }
+//             l++;
+//         }
+        
+//         return root;
+//     }
+    
     TreeNode* replaceValueInTree(TreeNode* root) {
-        queue<pp> q;
-        q.push({root, -1});
         
-        unordered_map<int, unordered_map<int,int>> mp;
-        
-        int l=0, no=-1;
-        
-        vector<int> lvls;
-        
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        int x=0;
         while(!q.empty()){
-            
-            int n= q.size(), sum=0;
-            
+            int total=0, n= q.size();
+            vector<pair<TreeNode*, int>> temp;
+            unordered_map<int,int> mp;
             for(int i=0; i<n; i++){
-                TreeNode* a= q.front().first;
-                int b= q.front().second;
-               
-                mp[l][b]+= a->val;
-                
-                sum+= a->val;
-                no++;
-                if(a->left!=NULL) q.push({a->left, no});
-                if(a->right!=NULL) q.push({a->right, no});
+                TreeNode* node= q.front().first;
+                int parent= q.front().second;
                 q.pop();
-                
+                x++;
+                total+= node->val;
+                mp[parent]+= node->val;
+                temp.push_back({node,parent});
+                if(node->left) q.push({node->left,x});
+                if(node->right) q.push({node->right, x});
             }
-            lvls.push_back(sum);
-            l++;
-        }
-        
-        q.push({root, -1});
-        l=0, no=-1;
-        while(!q.empty()){
-            int n= q.size(), sum=0;
             
-            for(int i=0; i<n; i++){
-                TreeNode* a= q.front().first;
-                int b= q.front().second;
-               
-                a->val= lvls[l]- mp[l][b];
-                no++;
-                if(a->left!=NULL) q.push({a->left, no});
-                if(a->right!=NULL) q.push({a->right, no});
-                q.pop();
+            for(auto &i: temp){
+                TreeNode * node= i.first;
+                int parent= i.second;
+                int newVal= total- mp[parent];
+                node->val= newVal;
             }
-            l++;
         }
-        
         return root;
     }
 };
