3ff17b24fcdfcd2da0b5dacbdefc272737f1df0a
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-05-22 14:19:08 +0530

    Time: 11 ms (96.11%), Space: 15.5 MB (7.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,40 +1,56 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-//         unordered_map<int, unordered_map<int,bool>> mp;
-//         unordered_map<int,int> cnts;
-//         int maxcnt=0;
-//         for(int i=0; i<nums.size(); i++){
-//             cnts[nums[i]]++;
-//             int x= cnts[nums[i]]-1;
-//             mp[x].erase(nums[i]);
-//             mp[x+1][nums[i]]=true;
-//             maxcnt= max(maxcnt, x+1);
-//         }
-//         vector<int> ans;
-//         while(k>0){
-//             for(auto i: mp[maxcnt]){
-//                 ans.push_back(i.first);
-//                 k--;
-//                 if(k==0) return ans;
-//             }
-//             maxcnt--;
-//         }
+        // unordered_map<int, unordered_map<int,bool>> mp;
+        // unordered_map<int,int> cnts;
+        // int maxcnt=0;
+        // for(int i=0; i<nums.size(); i++){
+        //     cnts[nums[i]]++;
+        //     int x= cnts[nums[i]]-1;
+        //     mp[x].erase(nums[i]);
+        //     mp[x+1][nums[i]]=true;
+        //     maxcnt= max(maxcnt, x+1);
+        // }
+        // vector<int> ans;
+        // while(k>0){
+        //     for(auto &i: mp[maxcnt]){
+        //         ans.push_back(i.first);
+        //         k--;
+        //         if(k==0) return ans;
+        //     }
+        //     maxcnt--;
+        // }
         
-//         return ans;
-        unordered_map<int,int>mp;
-        for(auto i: nums) mp[i]++;
-        priority_queue<pair<int,int>> pq;
-        for(auto i= mp.begin(); i!= mp.end(); i++){
-            pq.push(make_pair(i->second,i->first));
-        }
+        // return ans;
+        // unordered_map<int,int>mp;
+        // for(auto i: nums) mp[i]++;
+        // priority_queue<pair<int,int>> pq;
+        // for(auto i= mp.begin(); i!= mp.end(); i++){
+        //     pq.push(make_pair(i->second,i->first));
+        // }
+        // vector<int> ans;
+        // while(k-- && !pq.empty()){
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // return ans;
+        unordered_map<int, int> m;
+        for (int num : nums)
+            ++m[num];
+        
+        vector<vector<int>> buckets(nums.size() + 1); 
+        for (auto p : m)
+            buckets[p.second].push_back(p.first);
+        
         vector<int> ans;
-        while(k-- && !pq.empty()){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        for (int i = buckets.size() - 1; i >= 0 && ans.size() < k; --i) {
+            for (int num : buckets[i]) {
+                ans.push_back(num);
+                if (ans.size() == k)
+                    break;
+            }
         }
         return ans;
-        
 
     }
     
