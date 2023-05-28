a028d8b84316a671827f776e4a568e247a4aa036
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-05-28 20:08:03 +0530

    Time: 100 ms (36.83%), Space: 10.4 MB (32.53%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -14,7 +14,17 @@ public:
         cuts.insert(cuts.begin(), 0);
         cuts.push_back(n);
         sort(cuts.begin(), cuts.end());
-        vector<vector<int>> dp(cuts.size()+1, vector<int>(cuts.size()+1, -1));
-        return helper(1, cuts.size() -2, cuts, dp);
+        // vector<vector<int>> dp(cuts.size()+1, vector<int>(cuts.size()+1, -1));
+        // return helper(1, cuts.size() -2, cuts, dp);
+        int c= cuts.size();
+        vector<vector<int>> dp(c+1, vector<int>(c+1, 0));
+        for(int i=c-2; i>=1; i--){
+            for(int j=1; j<=c-2; j++){
+                for(int idx= i; idx<=j; idx++){
+                    dp[i][j] = min((dp[i][j]==0)? INT_MAX:dp[i][j], cuts[j+1]-cuts[i-1] + dp[i][idx-1] + dp[idx+1][j]);
+                }
+            }
+        }
+        return dp[1][c-2];
     }
 };
