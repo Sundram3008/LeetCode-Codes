d1bdf4dcb73ac63177e87a5285fa4207bcd8109f
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-06-11 18:01:58 +0530

    Time: 19 ms (80.00%), Space: 6.2 MB (60.00%) - LeetHub

--- 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.cpp
+++ 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.cpp
@@ -3,16 +3,11 @@ public:
     int longestSemiRepetitiveSubstring(string s) {
         int n= s.length();
         if(n<=2) return n;
-        int l=0, ans=0, cnt=0, prev=0, prev2=0;
+        int l=0, ans=0, prev=0;
         for(int i=1; i<n; i++){
             if(s[i]==s[i-1]){
-                prev2=prev;
-                prev=i;
-                cnt++;
-            }
-            if(cnt>1){
-                l=prev2;
-                cnt=1;
+                if(prev) l=prev;
+                prev= i;
             }
             ans= max(ans, i-l+1);
         }
