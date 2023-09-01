87a552c4fc3b0cd11f96b132dfbf6fd553a5371f
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-09-01 13:18:32 +0530

    Time: 3 ms (89.82%), Space: 7.9 MB (51.17%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1, 0);
-        int l=1, b=1;
+        int l=1;
         for(int i=1; i<=n; i++){
-            if(i && (!(i & (i - 1)))){
-                ans[i]=1;
-                b=i;
-                l=1;
-            }else ans[i]+= ans[b]+ans[l++];
+            if(i && (!(i & (i - 1)))) l=i;
+            ans[i]= ans[i%l]+1;
         }
         return ans;
     }
