c4ead55e9ebd9c906436c2356c3b142f5fa45588
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-09-28 22:53:40 +0530

    Time: 9 ms (66.67%), Space: 44.6 MB (50.00%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2531-make-number-of-distinct-characters-equal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2531-make-number-of-distinct-characters-equal.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2531-make-number-of-distinct-characters-equal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2531-make-number-of-distinct-characters-equal.java
@@ -5,33 +5,38 @@ class Solution {
         }
         return;
     }
-    boolean valid(int []a, int[]b){
-        int u1=0, u2=0;
-        for(int i=0; i<26; i++){
-            if(a[i]>0) u1++;
-            if(b[i]>0) u2++;
-        }
-        return (u1==u2);
-    }
     public boolean isItPossible(String word1, String word2) {
         int fw1[] = new int[26], fw2[] = new int[26];
         freqFiller(fw1, word1);
         freqFiller(fw2, word2);
+        int u1=0, u2=0;
+        for(int i=0; i<26; i++){
+            if(fw1[i]>0) u1++;
+            if(fw2[i]>0) u2++;
+        }
         for(int i=0; i<26; i++){
             if(fw1[i]>0){
                 fw1[i]--;
+                if(fw1[i]==0) u1--;
                 for(int j=0; j<26; j++){
                     if(fw2[j]>0){
                         fw2[j]--;
+                        if(fw2[j]==0) u2--;
                         fw1[j]++;
+                        if(fw1[j]==1) u1++;
                         fw2[i]++;
-                        if(valid(fw1, fw2)) return true;
+                        if(fw2[i]==1) u2++;
+                        if(u1==u2) return true;
                         fw2[j]++;
+                        if(fw2[j]==1) u2++;
                         fw1[j]--;
+                        if(fw1[j]==0) u1--;
                         fw2[i]--;
+                        if(fw2[i]==0) u2--;
                     }
                 }
                 fw1[i]++;
+                if(fw1[i]==1) u1++;
             }
         }
         return false;
