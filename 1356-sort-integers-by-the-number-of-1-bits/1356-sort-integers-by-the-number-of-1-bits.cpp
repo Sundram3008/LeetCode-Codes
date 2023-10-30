bf5a3ad8c29ddec078dae3c7226f1c03e0902d1d
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-10-30 21:36:41 +0530

    Time: 3 ms (96.58%), Space: 11 MB (29.55%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    static bool myComp(pair<int, int> &a, pair<int, int> &b){
+        if(a.second != b.second) return a.second< b.second;
+        return a.first < b.first;
+    }
     vector<int> sortByBits(vector<int>& arr) {
-        map<int, multiset<int>> mp;
+        vector<pair<int, int>> bav;
         for(int i=0; i<arr.size(); i++){
-            mp[__builtin_popcount(arr[i])].insert(arr[i]);
+            bav.push_back({arr[i], __builtin_popcount(arr[i])});
         }
+        sort(bav.begin(), bav.end(), myComp);
         vector<int> ans;
-        for(auto i: mp){
-            for(auto j: i.second) ans.push_back(j);
-        }
+        for(int i=0; i<bav.size(); i++) ans.push_back(bav[i].first);
         return ans;
     }
 };
