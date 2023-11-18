65a78ee85d3d36f77c3f034b991d264a213d1b8e
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2023-11-18 23:28:17 +0530

    Time: 32 ms (13.44%), Space: 61.9 MB (6.16%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -4,16 +4,13 @@ class Solution {
         int res=1;
         int pos=nums.length-1, curr=k, sum=0;
         for(int i=nums.length-1; i>=0 && pos>0; i--){
-            if(i!= nums.length-1 && nums[i+1]-nums[i]<=k){
-                curr+= ((i+1-pos)*nums[i+1] - sum)-(((i-pos)*nums[i])- (sum- nums[i]));
-                sum-= nums[i];
-            }
+            if(i!= nums.length-1) curr+= ((i+1-pos)*nums[i+1] - sum-nums[i])-(((i-pos)*nums[i])- sum);
             while(pos-1>=0 && nums[i]-nums[pos-1]<= curr){
-                sum+= nums[pos-1];
-                curr-= (nums[i]-nums[pos-1]);
-                pos--;
+                sum+= nums[--pos];
+                curr-= (nums[i]-nums[pos]);
             }
             res= Math.max(res,i-pos+1);
+            sum-= nums[i];
         }
         return res;
     }
