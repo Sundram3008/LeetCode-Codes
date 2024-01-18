8882066fba36e60ff53934c4c1f69d22153de1c7
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2024-01-18 11:42:20 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (8.31%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int prev1 =1, prev2=1;
+        for(int i=2; i<=n; i++){
+            int temp = prev1;
+            prev1+= prev2;
+            prev2=temp;
+        }
+        return prev1;
+    }
+}
