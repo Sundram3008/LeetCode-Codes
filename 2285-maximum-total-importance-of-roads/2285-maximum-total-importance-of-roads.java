74488af1a0999656cc59116495643a7d5ca86a5b
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2024-06-29 01:24:48 +0530

    Time: 6 ms (98.40%), Space: 63.2 MB (45.45%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -1,15 +1,9 @@
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        
-        for(int i=0; i<roads.length; i++){
-            mp.put(roads[i][0], mp.getOrDefault(roads[i][0], 0)+1);
-            mp.put(roads[i][1], mp.getOrDefault(roads[i][1], 0)+1);
-        }
-        
         long []vals = new long[n];
-        for(Map.Entry<Integer, Integer> x: mp.entrySet()){
-            vals[x.getKey()]= x.getValue();
+        for(int i=0; i<roads.length; i++){
+            vals[roads[i][0]]++;
+            vals[roads[i][1]]++;
         }
         Arrays.sort(vals);
         long res=0;
