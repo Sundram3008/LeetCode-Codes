2afc7e0d8f7aa509e67147cfe265aef2c68f544e
Sundram3008 <96676926+Sundram3008@users.noreply.github.com>
2024-08-02 00:52:48 +0530

    Time: 12 ms (13.14%), Space: 17.7 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,8 +3,7 @@ public:
     int countSeniors(vector<string>& details) {
         int res=0;
         for(auto i: details){
-            int age= stoi(i.substr(11));
-            age/=100;
+            int age=( i[11]-'0')*10 + i[12]- '0';
             if(age>60) res++;
         }
         return res;
